db3486cb929aad29017e32fc66cc935b12db3577
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2015-10-08 16:14:15 -0300

    adapted to work with agenda sheet

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,9 @@
 #encoding: utf-8
 require 'scraperwiki'
 require 'nokogiri'
-require 'json'
 
 # Read in a page
-url = "https://docs.google.com/spreadsheets/d/1QkkIRF-3Qrz-aRIxERbGbB7YHWz2-t4ix-7TEcuBNfE/pubhtml?gid=607896942&single=true"
+url = "https://docs.google.com/spreadsheets/d/1QkkIRF-3Qrz-aRIxERbGbB7YHWz2-t4ix-7TEcuBNfE/pubhtml?gid=2014567304&single=true"
 page = Nokogiri::HTML(open(url), nil, 'utf-8')
 rows = page.xpath('//table[@class="waffle"]/tbody/tr')
 
@@ -18,29 +17,22 @@ end
 content.shift
 content.each do |row|
 
-  tags = []
-  row[3].gsub(', ',',').split(',').each do |tag|
-    tags << tag.strip
-  end
-
   record = {
-    "date" => row[0],
-    "title" => row[1],
-    "summary" => row[2],
-    "tags" => tags,
-    "source" => row[4],
-    "img" => row[5],
-    "highlighted" => row[6],
+    "id" => row[0],
+    "date" => row[1],
+    "startDate" => row[2],
+    "endDate" => row[3],
+    "title" => row[4],
+    "summary" => row[5],
     "last_update" => Date.today.to_s
   }
 
   # Save if the record doesn't exist
-  if ((ScraperWiki.select("* from data where `source`='#{record['source']}'").empty?) rescue true)
-    record['tags'] = JSON.dump(record['tags'])
-    ScraperWiki.save_sqlite(["source"], record)
-    puts "Adds new record from " + record['source']
+  if ((ScraperWiki.select("* from data where `source`='#{record['id']}'").empty?) rescue true)
+    ScraperWiki.save_sqlite(["id"], record)
+    puts "Adds new record " + record['id']
   else
-    puts "Skipping already saved record from " + record['source']
+    puts "Skipping already saved record " + record['id']
   end
 end
 
